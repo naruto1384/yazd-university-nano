f7e3b0e57791c9b4657db78e490afd0bb8e45795
naruto1384 <arash09928025189@gmail.com>
2026-05-31 08:17:23 -0700

    Update theme.js

--- reports/js/theme.js
+++ reports/js/theme.js
@@ -3,20 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.querySelector(".theme-toggle");
   const savedTheme = localStorage.getItem("theme");
 
-  // اگر کاربر قبلاً تم انتخاب کرده باشد
   if (savedTheme === "dark") {
     body.classList.add("dark");
   } else if (savedTheme === "light") {
     body.classList.remove("dark");
   } else {
-    // اگر تم ذخیره نشده بود، تم سیستم بررسی شود
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
     if (prefersDark) {
       body.classList.add("dark");
     }
   }
 
-  // تغییر تم با کلیک روی دکمه
   if (toggleButton) {
     toggleButton.addEventListener("click", function () {
       body.classList.toggle("dark");
